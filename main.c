77264a6ce7b81a1372f58999dd8c9a2d8326b17b
lawrence <lshek50@stuy.edu>
2024-11-26 12:27:51 -0500

    added children

--- main.c
+++ main.c
@@ -5,15 +5,27 @@
 
 
 int main() {
-  pid_t p;
-  p = fork();
-  if(p<0){
+  pid_t p1, p2;
+  printf("%d about to create 2 child processes\n", getpid());
+  p1 = fork();
+  if(p1 < 0){
     perror("fork fail");
     exit(1);
-  } else if ( p == 0){
-      printf("Hello from Child!\n");
+  } else if ( p1 == 0){
+      // printf("Hello from Child 1!\n");
+      printf("%d\n", getpid());
   }else{
-      printf("Hello from Parent!\n");
+      // printf("Hello from Parent of child 1!\n");
+      p2 = fork();
+      if(p2 < 0){
+        perror("fork fail");
+        exit(1);
+      } else if ( p2 == 0){
+          // printf("Hello from Child 2!\n");
+          printf("%d\n", getpid());
+      }else{
+          // printf("Hello from Parent of child 2!\n");
+      }
   }
   return 0;
 }
